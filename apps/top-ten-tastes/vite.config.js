5d578e151bc0bb51baa4eee0cd17046ad3f9e8da
Cassidy Johnson <crjohnson@legacyscape.com>
2024-09-09 18:05:58 -0500

    updated api key usage for use with turbo

--- apps/top-ten-tastes/vite.config.js
+++ apps/top-ten-tastes/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/toptentastes/'
+    base: '/toptentastes/',
+    define: {
+        'import.meta.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.VITE_GOOGLE_MAPS_API_KEY)
+    }
 });
